262a9b0706a25173513c9fa0eac8a250db0845d1
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-08-01 22:18:29 +0100

    add homework without styles

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,15 @@
-const FriendList = () => {
-  return <>FriendList</>;
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default FriendList;
